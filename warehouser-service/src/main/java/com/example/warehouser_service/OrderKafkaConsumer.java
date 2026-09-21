37ee95a65c40b7817a049b0455cb1e58479344f6
tsurinoruri <olegivanov74754@gmail.com>
2026-09-21 19:45:58 +0300

    commit

--- warehouser-service/src/main/java/com/example/warehouser_service/OrderKafkaConsumer.java
+++ warehouser-service/src/main/java/com/example/warehouser_service/OrderKafkaConsumer.java
@@ -1,6 +1,7 @@
 package com.example.warehouser_service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +11,8 @@ public class OrderKafkaConsumer {
 
     //указываем какой топик слушаем
     @KafkaListener(topics = "orders")
-    public void consumeOrder(Order order){
-        log.info("Receive order");
+    public void consumeOrder(ConsumerRecord<String, Order> record){
+
+        log.info("Receive order={}, key={}, partition={}",record.value(), record.key(), record.partition());
     }
 }
